f799d7b1c0273f9ce7d36914420d084a25a23caf
Yifan Yang <yyangdev@gmail.com>
2019-06-05 00:30:00 -0400

    Added commands sheet.

--- train.py
+++ train.py
@@ -32,8 +32,6 @@ import mrcnn.model as modellib
 from mrcnn import visualize
 from mrcnn.model import log
 
-get_ipython().run_line_magic('matplotlib', 'inline')
-
 # Directory to save logs and trained model
 MODEL_DIR = os.path.join(ROOT_DIR, "logs")
 
